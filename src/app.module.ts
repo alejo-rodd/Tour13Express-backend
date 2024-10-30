dd271f16fb7f9b4f1b0760b0098fb5ff777165a9
alejo-rodd <alejordog@outlook.com>
2024-10-29 22:44:16 -0500

    create project

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { CommonModule } from './common/common.module';
+
+@Module({
+  imports: [ConfigModule.forRoot(), CommonModule, ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
